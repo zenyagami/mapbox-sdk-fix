6780e366a2f43522ab936b718445bdb90b21df08
Javier <zenyagami@gmail.com>
2023-04-16 17:53:02 +0200

    added first commit for mapbox sk fix for android 14

---
+++ MapboxGLAndroidSDK/build/generated/source/buildConfig/debug/com/mapbox/mapboxsdk/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mapbox.mapboxsdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.mapbox.mapboxsdk";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String GIT_REVISION_SHORT = "";
+  // Field from default config.
+  public static final String MAPBOX_EVENTS_USER_AGENT = "mapbox-maps-android/9.6.0-alpha.1";
+  // Field from default config.
+  public static final String MAPBOX_SDK_IDENTIFIER = "mapbox-maps-android";
+  // Field from default config.
+  public static final String MAPBOX_SDK_VERSION = "9.6.0-alpha.1";
+  // Field from default config.
+  public static final String MAPBOX_VERSION_STRING = "Mapbox/9.6.0-alpha.1";
+}
---
+++ MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/location/MapboxCameraAnimatorAdapter.java
@@ -0,0 +1,16 @@
+package com.mapbox.mapboxsdk.location;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.Size;
+
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+class MapboxCameraAnimatorAdapter extends MapboxFloatAnimator {
+  MapboxCameraAnimatorAdapter(@NonNull @Size(min = 2) Float[] values,
+      AnimationsValueChangeListener updateListener,
+      @Nullable MapboxMap.CancelableCallback cancelableCallback) {
+    super(values, updateListener, Integer.MAX_VALUE);
+    addListener(new MapboxAnimatorListener(cancelableCallback));
+  }
+}
