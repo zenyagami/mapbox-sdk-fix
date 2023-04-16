b1fb370f6a9850490fa17e94a66eb41f7fd1e3d5
Javier <zenyagami@gmail.com>
2023-04-16 18:35:31 +0200

    Fixed .gitignore to ignore build dir added maven publish support

--- MapboxGLAndroidSDK/build/generated/source/buildConfig/debug/com/mapbox/mapboxsdk/BuildConfig.java
+++ MapboxGLAndroidSDK/build/generated/source/buildConfig/debug/com/mapbox/mapboxsdk/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.mapbox.mapboxsdk";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String GIT_REVISION_SHORT = "";
+  public static final String GIT_REVISION_SHORT = "71b587c";
   // Field from default config.
   public static final String MAPBOX_EVENTS_USER_AGENT = "mapbox-maps-android/9.6.0-alpha.1";
   // Field from default config.
